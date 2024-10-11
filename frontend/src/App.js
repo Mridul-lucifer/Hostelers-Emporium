161a7ba6d3251671d8cd6675ab646c0b6c3b01da
Mridul-lucifer <124557704+Mridul-lucifer@users.noreply.github.com>
2024-10-11 13:41:06 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import LoginPage from './Components/LoginPage'
+import Signup from './Components/Signup'
+import HomePage from './Components/HomePage'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+// import SellProduct from './Components/SellProduct'
+import Sell from './Components/sell/sell'
+// import BuyProduct from './Components/BuyProduct'
+import Buy from './Components/Buy/Buy'
+// import YourProducts from './Components/YourProducts'
+import YourProducts from './Components/YourProducts/YourProducts'
+import UpdateProfile from './Components/UpdateProfile'
+import ChangePassword from './Components/ChangePassword'
+import DeleteProfile from './Components/DeleteProfile'
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element :<LoginPage/>
+    },
+    {
+      path:"/Login",
+      element :<LoginPage/>
+    },
+    {
+      path:"/Signup",
+      element : <Signup/>
+    },
+    {
+      path:"/Home",
+      element : <><Header/><HomePage/><Footer/></>
+    },
+    {
+      path:"/UpdateProfile",
+      element: <><Header/><UpdateProfile/><Footer/></>
+    },
+    {
+      path : "/SellProduct",
+      element : <><Header/><Sell/><Footer/></>
+    },
+    {
+      path : "/BuyProducts",
+      element : <><Header/><Buy/><Footer/></>
+    },
+    {
+      path : "/YourProducts",
+      element : <><Header/><YourProducts/><Footer/></>
+    },
+    {
+      path : "/ChangePassword",
+      element : <><Header/><ChangePassword/><Footer/></>
+    },
+    {
+      path : "/DeleteProfile",
+      element : <><Header/><DeleteProfile/><Footer/></>
+    }
+  ])
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  )
+}
